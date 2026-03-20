d30401f5bf3e0c019e20466a7db6747e2e2e6e91
Rafaan Hyder <rafaan.hyder@gmail.com>
2026-03-19 23:47:51 -0700

    created an nba bets page with espn API

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,10 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function Sidebar() {
   return (
     <aside className="sidebar">
       <h2 className="sidebar-title">Leagues</h2>
 
       <ul className="sidebar-list">
-        <li>🏀 NBA</li>
+        <li>
+          <Link to="/nba" className="no-underline">
+            🏀 NBA
+          </Link>
+        </li>
         <li>🏈 NFL</li>
         <li>⚾ MLB</li>
         <li>🏒 NHL</li>
